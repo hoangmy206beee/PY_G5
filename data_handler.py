f3a055cc64cc6e18f0e0a823ab3651e2c2e357b1
loantran06-it <loantta4495@ut.edu.vn>
2026-06-20 22:32:04 +0700

    Ánh Loan: Hoàn thành file data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,40 +1,23 @@
 import os
-<<<<<<< HEAD
 
 TXT_FILE_PATH = "data.txt"
 
 def read_txt_data():
     # Kiểm tra nếu file txt chưa tồn tại thì trả về danh sách rỗng
-=======
-TXT_FILE_PATH = "data.txt"
-
-def read_txt_data():
->>>>>>> b9b71646adeb35b7c632caf0c2df1f7df29dce87
     if not os.path.exists(TXT_FILE_PATH):
         return []
     try:
         with open(TXT_FILE_PATH, 'r', encoding='utf-8') as f:
-<<<<<<< HEAD
             # Đọc từng dòng, xóa khoảng trắng thừa và loại bỏ dòng trống
             raw_list = [line.strip() for line in f if line.strip()]
-        
+
         # Chỉ lấy 10 từ khóa mới nhất và đảo ngược để từ mới nhất lên đầu
-=======
-            raw_list = [line.strip() for line in f if line.strip()]
-            
->>>>>>> b9b71646adeb35b7c632caf0c2df1f7df29dce87
         return raw_list[-10:][::-1]
     except Exception as e:
         return []
 
 def save_txt_data(history_list):
-<<<<<<< HEAD
     # Mở file txt ở chế độ ghi đè 'w' để cập nhật danh sách mới nhất
     with open(TXT_FILE_PATH, 'w', encoding='utf-8') as f:
         for item in history_list:
             f.write(f"{item}\n")
-=======
-    with open(TXT_FILE_PATH, 'w', encoding='utf-8') as f:
-        for item in history_list:
-            f.write(f"{item}\n")
->>>>>>> b9b71646adeb35b7c632caf0c2df1f7df29dce87
